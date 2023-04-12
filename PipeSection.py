5c8967df1fe0cb400213e4f01194969b2f94e9fc
KyleM8 <kylejm220@gmail.com>
2023-04-12 02:28:06 -0500

    dQ and Q calculations, refactoring
    Refactored the project slightly by modifying a few class structures to potentially make it easier to implement the input and output flows in the future.
    
    Moved all the code from the HardyCross class into the main method to make the necessary global variables work easier.
    
    To do: Ensure that hL converges and is being recorded properly (hL calculation is not currently working correctly). Implement input and output flows (mass balance).

--- PipeSection.py
+++ PipeSection.py
@@ -1,26 +1,32 @@
 import math #math import so we can use the pi value
+#from decimal import Decimal
 
 class PipeSection:
 
-    def __init__(self, n, fF, sL, d, iRG):
+    def __init__(self, pP1, pP2, fF, sL, d, iRG):
         #identifying information for each pipe section given in problem statement
-        self.name = n #string to identify the pipe section, e.g. "AB"
+        self.pipePoints = [pP1, pP2]  #list of two PipePoint objects to define where the pipe is located
         self.frictionFactor = fF #friction factor of this pipe section
         self.sectionLength = sL #length of this pipe section
         self.diameter = d #diameter of this pipe section
         self.initialRateGuess = iRG #initial flow rate guess
 
         #data we need to keep track of for each object for each iteration 
-        self.headLossHistory = [] #list of the iterated values of head loss
-        self.qHistory = [] #list of the iterated values of Q (each time we calculate a dQ, we add that dQ and add a new item to the list)
-        self.qHistory.append(iRG) #adds the initial flow rate guess as the first iterated value of Q
-        self.dQ = 0.0
+        self.headLossHistory = [float(0.0)] #list of the iterated values of head loss
+        self.qHistory = [float(0.0)] #list of the iterated values of Q (each time we calculate a dQ, we add that dQ and add a new item to the list)
+        self.qHistory.append(float(iRG)) #adds the initial flow rate guess as the first iterated value of Q
+        self.dQHistory = [float(0.0)]
 
         #calculate and define the constant k from the data given
         self.kConst = (8*fF*sL)/(9.81*pow(math.pi,2)*pow(d,5))
+        #self.kConst = decimal.Decimal.divide(Decimal.multiply(8,Decimal.multiply(fF,sL)), Decimal.multiply(9.81,Decimal.multiply(Decimal.power(math.pi,2), Decimal.power(d,5))))
+
+        #add a "reversed" marker; false by default
+        #indicates to other methods if the PipeSection in question is being viewed as flowing forwards or backwards
+        self.reversed = False
 
     def printData(self):
-        print("name: " + self.name)
+        print("name: " + self.pipePoints[0].getName() + self.pipePoints[1].getName())
         print("friction factor: " + str(self.frictionFactor))
         print("section length: " + str(self.sectionLength))
         print("diameter: " + str(self.diameter))
@@ -30,8 +36,8 @@ class PipeSection:
     def getKConst(self):
         return self.kConst
 
-    def getName(self):
-        return self.name
+    def getPipePoints(self):
+        return self.pipePoints
     
     def appendFlowRateHistory(self,num):
         self.flowRateHistory.append(num)
@@ -66,8 +72,17 @@ class PipeSection:
     def getFullQHistory(self):
         return self.qHistory
     
-    def getDQ(self):
-        return self.dQ
+    def getRecentDQ(self):
+        return self.dQ[len(self.dQ)-1]
 
-    def setDQ(self,num):
-        self.dQ = num
+    def appendDQHistory(self,num):
+        self.dQHistory.append(num)
+    
+    def getDQHistory(self):
+        return self.dQHistory
+    
+    def getReversed(self):
+        return self.reversed
+
+    def setReversed(self, r):
+        self.reversed = r
